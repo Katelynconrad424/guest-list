fb2011b880b53b0a78b2324177f2af88f8eaa6db
Katelynconrad424 <katelynconrad424@gmail.com>
2025-11-12 10:49:21 -0500

    Final Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,15 @@
+import { useState } from "react";
+import GuestDetails from "./guests/GuestDetails";
+import GuestList from "./guests/GuestList";
+
 export default function App() {
-  return <></>;
+  const [selectedGuest, setSelectedGuest] = useState(null);
+
+  return (
+    <div>
+      <h1>Guest List</h1>
+      <GuestList setSelectedGuest={setSelectedGuest} />
+      {selectedGuest && <GuestDetails guest={selectedGuest} />}
+    </div>
+  );
 }
